a5a761e655dd3a9f32431716179321643b72536d
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-07-18 23:11:53 +0530

    make character movement

---
+++ Assets/scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float mouseSensitivly = 100f;
+    public Transform playerBody;
+    float xRotation =0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivly * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivly * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -70f, 70f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
